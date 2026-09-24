2e653dce3046d3170a65d10882410017a78bd908
phongtran123456 <trandotienphong.edu.vn@gmail.com>
2026-09-24 09:42:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ function setupPhongOptionHandler() {
       }, 4000);
     } else {
       // Khi bỏ chọn: trả lại tên ban đầu
-      phongSpan.innerText = "Thi chọn cho mình đi :3";
-      phongInput.value = "Thi chọn cho mình đi :3";
+      phongSpan.innerText = "Phong tự chọn luôn đi";
+      phongInput.value = "Phong tự chọn luôn đi";
     }
   });
 }
